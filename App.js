a899e3112eac5b22f69633fbad8dbb5bfdd64487
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-02-09 16:42:50 -0300

    Inicializando componente Home

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up Afpp.js to start working on your app!</Text>
-      <Text>Olasdaaaaaaaaaaaaaaa</Text>
+      <Text>Hello Mobile</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
