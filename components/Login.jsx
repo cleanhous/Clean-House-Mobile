7b0fd19ef9895de62d1c6447ab85089a285502c8
afonso7ss <afonso.santos7ass@gmail.com>
2025-03-17 11:06:55 -0300

    update telaLogin

--- components/Login.jsx
+++ components/Login.jsx
@@ -11,10 +11,12 @@ import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import api from "../services/api";
 import Logo from "../assets/logo.svg";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -23,16 +25,12 @@ const Login = () => {
   const handleSubmit = async () => {
     setIsLoading(true);
     setErrorMessage("");
-    navigation.replace("Navbar"); 
-
 
     try {
       const response = await api.post("/login", { email, senha });
       const { acessToken } = response.data;
-
       await AsyncStorage.setItem("acessToken", acessToken);
-
-      navigation.replace("Navbar"); 
+      navigation.replace("Navbar");
     } catch (error) {
       if (error.response) {
         setErrorMessage(error.response.data.message || "Erro ao fazer login");
@@ -57,13 +55,25 @@ const Login = () => {
         onChangeText={setEmail}
       />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        secureTextEntry
-        value={senha}
-        onChangeText={setSenha}
-      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Senha"
+          secureTextEntry={!showPassword}
+          value={senha}
+          onChangeText={setSenha}
+        />
+        <TouchableOpacity
+          onPress={() => setShowPassword(!showPassword)}
+          style={styles.iconContainer}
+        >
+          <Icon
+            name={showPassword ? "eye" : "eye-slash"}
+            size={20}
+            color="gray"
+          />
+        </TouchableOpacity>
+      </View>
 
       {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
 
@@ -110,7 +120,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
-    width: "95%",
+    width: "100%",
     padding: 10,
     backgroundColor: "#fff",
     color: "#0369A1",
@@ -118,6 +128,25 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderWidth: 1,
     borderColor: "#0369A1",
+  },
+  passwordContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#0369A1",
+    marginBottom: 10,
+  },
+  passwordInput: {
+    flex: 1,
+    padding: 10,
+    color: "#0369A1",
+    borderWidth: 0, // Sem borda no TextInput
+  },
+  iconContainer: {
+    padding: 10,
   },
   button: {
     width: "95%",
